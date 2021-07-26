f490001d293d63005bd343db30dbe9250359818b
ConnorH14 <ConnorH14@live.com>
2021-07-26 10:01:38 -0600

    init

---
+++ trip.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:5001' : ''
+export const domain = ''
+export const audience = ''
+export const clientId = ''
